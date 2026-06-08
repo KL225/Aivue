9591a9528239260c6e4a7839b549d9656db96a27
KL225 <2023836425@qq.com>
2026-06-08 11:38:32 +0800

    Fix router guard logic

--- src/router/index.js
+++ src/router/index.js
@@ -99,14 +99,16 @@ const router = createRouter({
 // 路由前置守卫
 router.beforeEach((to, from, next) => {
     const token = localStorage.getItem('token')
-    const userInfo = JSON.parse(localStorage.getItem('userInfo'))
-    if (token) {
+    const userInfoStr = localStorage.getItem('userInfo')
+    const userInfo = userInfoStr ? JSON.parse(userInfoStr) : null
+
+    if (token && userInfo) {
         // 如果是后台用户
         if (userInfo.userType === 2) {
             if (to.path.startsWith('/back')) {
                 next()
             } else {
-                next( '/back/bashboard' )
+                next('/back/bashboard')
             }
         } else if (userInfo.userType === 1) {
             // 用户端的账号只能访问前台路由
@@ -115,11 +117,18 @@ router.beforeEach((to, from, next) => {
             } else {
                 next()
             }
+        } else {
+            next()
         }
     } else {
+        // 清除无效的登录信息
+        if (token && !userInfo) {
+            localStorage.removeItem('token')
+            localStorage.removeItem('userInfo')
+        }
         if (to.path.startsWith('/back')) {
             // 如果是访问后台页面，那么跳转到登录页
-            next( '/auth/login' )
+            next('/auth/login')
         } else {
             next()
         }
